c15deaf9a5330cb009d55072fe24afc9a0325ef5
WaiCat <rxg1123@gmail.com>
2023-11-03 17:33:37 +0900

    Floyd

--- Dijkstra/Dijkstra.py
+++ Dijkstra/Dijkstra.py
@@ -6,7 +6,7 @@ start = time.time()  # 실행 시작 시간 기록
 max_value = sys.maxsize  # 무한대 값으로 사용할 최대 정수값
 
 num_nodes = 10  # 그래프의 노드 수
-graph = [10, 14]  # (사용하지 않는 것으로 보이는) 그래프 속성
+graph = [10, 14]
 edges = [
     [0, 1, 15], 
     [1, 2, 21],
@@ -24,41 +24,38 @@ edges = [
     [7, 9, 5]
 ]
 
-# 각 노드에서 다른 모든 노드까지의 최단 거리를 계산
+# 간선 가중치를 저장할 2D 배열을 최댓값으로 초기화
+edge = [[max_value] * num_nodes for _ in range(num_nodes)]
+
+# 간선 가중치를 인접 행렬에 할당
+for i in edges:
+    u, v, w = i
+    edge[u][v] = edge[v][u] = w
+
+# Dijkstra 알고리즘 수행
 for k in range(num_nodes):
+    distances = [max_value] * num_nodes # 가중치 배열 초기화
+    distances[k] = 0 # 시작 노드 선정
+    visited = [False] * num_nodes # 방문 노드 배열
 
-    distances = [max_value] * num_nodes  # 모든 노드에 대한 거리를 무한대로 초기화
-    distances[k] = 0  # 시작 노드까지의 거리를 0으로 설정
-    visited = [False] * num_nodes  # 방문한 노드를 추적하기 위한 목록 초기화
-
-    # 방문하지 않은 노드가 있는지 확인하는 함수
-    def check():
-        return any(not visited[i] for i in range(num_nodes))
-
-    # 최소 거리를 가진 노드를 찾는 함수
-    def min_distance():
+    for _ in range(num_nodes):
         min_dist = max_value
         index = -1
 
-        for i in range(num_nodes):
+        for i in range(num_nodes):  # 최소 거리를 가진 노드를 찾음
             if not visited[i] and distances[i] < min_dist:
                 index = i
                 min_dist = distances[i]
-        return min_dist, index
 
-    while check():
-        min_dist, index = min_distance()  # 최소 거리를 가진 노드를 찾음
-        visited[index] = True  # 해당 노드를 방문한 것으로 표시
+        visited[index] = True # 최소 노드 방문
 
-        for edge in edges:
-            if edge[0] == index:
-                distances[edge[1]] = min(distances[edge[1]], min_dist + edge[2])  # 거리를 업데이트
-            if edge[1] == index:
-                distances[edge[0]] = min(distances[edge[0]], min_dist + edge[2])  # 거리를 업데이트
+        for i in range(num_nodes):  # 최소 노드에 연결된 노드로 가중치 갱신
+            if not visited[i] and edge[index][i] != max_value:
+                distances[i] = min(distances[i], min_dist + edge[index][i])
 
     for i in range(k + 1):
-        distances[i] = ' '  # 시작 노드 이전의 노드에 대한 자리 표시자
+        distances[i] = ''
     print(distances)  # 시작 노드에서 다른 모든 노드까지의 거리를 출력
 
-end = time.time()
-print("실행 시간:", (end - start)*1000)
+end = time.time() # 실행 종료 시간 기록
+print("실행 시간:", (end - start)) #실행시간 출력
