15084b0a0c3476cf5294cda4214a41f50318c96c
WaiCat <98246479+WaiCat@users.noreply.github.com>
2023-12-02 14:27:19 +0900

    TSP
    주석 정리

--- TSP/TSP.py
+++ TSP/TSP.py
@@ -9,14 +9,14 @@ adjacency_list = [[] for _ in range(num_vertices)]  # 정점의 이웃 정보를
 minimum_spanning_tree = []  # 최소 신장 트리를 구성하는 간선들의 리스트
 
 # 거리 계산 함수
-def calculate_distance(a, b):
+def calculate_distance(a, b): # 두점사이의 거리를 계산함
     x1, x2 = a[0], b[0]
     y1, y2 = a[1], b[1]
     distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
     return distance
 
-# 방문 순서 설정 함수
-def set_visit_order(start):
+# 방문 순서 설정 함수 # 재귀함수 사용
+ def set_visit_order(start): # 0에서 시작해서 다음점과 연결된 점들을 찾고 경로를 설정함
     l = len(minimum_spanning_tree)
     for _ in range(l):
         for tree in minimum_spanning_tree:
@@ -42,7 +42,7 @@ def is_cycle(s, e, visited):
                 return True
     return False
 
-# 거리 계산 및 heap 초기화
+# 각 점사이의 거리 계산 및 heap 초기화
 distances = []
 for i in range(len(points)):
     for j in range(i + 1, len(points)):
@@ -65,12 +65,11 @@ for i in range(num_edges):
 
 MST = [0]
 
-# 방문 순서 설정 및 거리 합산
-set_visit_order(0)
+set_visit_order(0) # 방문 순서 결정
 total_distance = 0
 index = 0
-for i in MST:
-    index = (index + 1) % len(MST)
+for i in MST: # 결정된 방문 순서에 따른 점사이의 거리 합산
+    index = (index + 1) % len(MST) 
     j = MST[index]
     total_distance += calculate_distance(points[i], points[j])
 
--- volume/volume.py
+++ volume/volume.py
@@ -1,4 +1,6 @@
+import os
 import csv
+import numpy as np
 
 def read_csv_file(file_path):
     data = []
@@ -13,16 +15,40 @@ def read_csv_file(file_path):
 def transpose_matrix(matrix):
     return list(map(list, zip(*matrix)))
 
+# 현재 스크립트 파일의 폴더 경로
+current_script_directory = os.path.dirname(os.path.abspath(__file__))
 # 파일 경로 설정
-file_path = 'C:\\Users\\Administrator\\Downloads\\University\\3학년 2학기\\Algorithm\\volune\\input.csv'
+file_path = current_script_directory + '\\input.csv'
 
 # CSV 파일 읽기
 csv_data = read_csv_file(file_path)
 
-# 행열을 반대로 저장
-transposed_data = transpose_matrix(csv_data)
+max_value = []
+for i in range(20):
+    max_value.append(max(csv_data[i]))
 
-# 결과 출력
-for row in transposed_data:
+hd = []
+
+print(max_value)
+
+for i in range(20):
+    num =csv_data[i].index(max_value[i])
+    tem = []
+    for j in range(20):
+        tem.append(csv_data[j][num])
+    hd.append(tem)
     
-    print(row)
+print(hd)
+
+hd = np.array(hd)
+hd = hd.astype(float)
+determinant_value = np.linalg.det(hd)
+
+print(determinant_value)
+# # 행열을 반대로 저장
+# transposed_data = transpose_matrix(csv_data)
+
+# # 결과 출력
+# for row in transposed_data:
+    
+#     print(row)
