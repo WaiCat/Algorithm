1440bc32fb15c8ed5de8aa473aa421df69dc48be
WaiCat <98246479+WaiCat@users.noreply.github.com>
2023-12-08 03:36:52 +0900

    vol

--- test.py
+++ test.py
@@ -1,24 +1,73 @@
+import pandas as pd
 import numpy as np
+import time
+from itertools import combinations 
 
-tmp = [
-    [1.057572462, 0.091304574, -1.941189358, 1.205990718, 1.184936214,
-    -0.0301019545, -0.0371822811, -1.23704029, -0.281044413, -1.620192061,
-    0.342326076, -0.869798621, -0.350382765, -0.406814212, -0.943408091,
-    1.024983718, -0.286866942, -1.518435212, 1.314715713, 1.23136462],
-    [0.139216337, 0.312966591, 0.634361253, 0.637731726, -0.147368595,
-    -0.0756256904, 0.244456388, -0.171410468, -0.69465666, 1.006488278,
-    0.909722485, 0.43884704, 1.412876138, 0.681612309, -0.496465006,
-    -1.191262983, 0.523378491, 0.608165799, -1.068301165, -0.0252068902]
-]
 
-tmp = np.array(tmp)
-tmp_t = np.transpose(tmp)
-result = np.dot(tmp_t, tmp)
 
-# det_result = np.linalg.det(result)
-# print(det_result)
-# print(np.sqrt(abs(det_result)))
+def get_matrix(path):
+    df = pd.read_csv(path) # Matrix Read 
+    df = np.array(df).T # Matrix Transpose 
+    return df
 
-matrix_product = np.dot(tmp, tmp_t)
-determinant_a = np.sqrt(abs(np.linalg.det(matrix_product)))
-print(determinant_a)
+def find_max(mat):
+    max_value = -1
+    max_idx = list()
+    max_li = list()
+    for i in range(10000):
+        if(len(max_idx) == 20):
+            break
+        find_max = -1
+        find_max_value = max_value
+        for j in range(10000):
+            if j not in max_idx:
+                tmp_li = max_li.copy()
+                tmp_li.insert(0, mat[j])
+                arr = np.array(tmp_li)
+                size = np.dot(arr, arr.T)
+                det = abs(np.linalg.det(size))
+                det = det **(1/2)
+                if det > find_max_value:
+                    find_max_value = det 
+                    find_max = j
+        if find_max != -1:
+            max_idx.insert(0, find_max)
+            max_li.insert(0, mat[find_max])
+            max_value = find_max_value
+            print(max_value, max_idx)
+            
+
+    return max_value, max_idx, max_li
+
+## 계산식
+# arr = np.array(tmp_li)
+# size = np.dot(arr, arr.T)
+# det = abs(np.linalg.det(size))
+# det = det **(1/2)
+
+max_row = []
+
+def calculate_determinant(combination):
+    tmp = np.array(combination)
+    tmp_t = np.transpose(tmp)
+    return np.sqrt(abs(np.linalg.det(np.dot(tmp, tmp_t))))
+
+if __name__ == "__main__":
+    start = time.thread_time_ns()
+    path = "volume/input.csv"
+    mat = get_matrix(path)
+    max_value, max_index, max_list = find_max(mat)
+    max_list = np.array(max_list)
+    for i in range(20):
+        max_index = np.argmax(max_list[:, i])
+        max_row.append(max_list[max_index, :])
+    print(max_list)
+    print(max_row)
+    print(calculate_determinant(max_row))
+    end = time.thread_time_ns()
+    times = (end-start)/1000
+    print(times)
+    print(max_value / times)
+    volume = 796727301049202.6
+    print(max_value-volume)
+
