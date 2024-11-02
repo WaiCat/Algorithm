214d499cb382813dc76f4075712c8df7ca96df66
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-11-03 00:18:19 +0900

    Create 2007.py

---
+++ Swea/2007.py
@@ -0,0 +1,27 @@
+t = int(input())
+l = 0
+
+for i in range(t):
+  arr = input()
+
+  li = arr[0:1]
+  l = len(li)
+  arr = arr[l:]
+  sub = ""
+
+  while(1):
+    index = arr.find(li)
+    if(len(arr)<=l):
+      break
+    if(index == 0):
+      sub += arr[:l]
+      arr = arr[l:]
+    else:
+      li = li + sub + arr[:index]
+      arr = arr[index:]
+      l = len(li)
+
+
+  print(li)
+
+  print("#" , (i+1) ," ", len(li) , sep = "")
