5b77e65c14f5edf10a54217a39020cc5b9d49400
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-11-09 16:06:12 +0900

    Update 1974.py

--- Swea/1974.py
+++ Swea/1974.py
@@ -5,38 +5,38 @@ def is_valid_matrix(matrix):
     
     flat_matrix_set = set()
     for row in matrix:
-        flat_matrix_set.update(row)
+        flat_matrix_set.add(row)
     
     return flat_matrix_set == expected_set
+
+def transpose(matrix):
+    return [list(row) for row in zip(*matrix)]
 
 for i in range(t):
   arr = [list(map(int, input().split())) for _ in range(9)]
   
   result = 1
   for j in range(9):
-    is_valid_matrix[arr[j]]
-  
-  if(result = 1):
-    for k in range(9):
-      false_list = [False] * 9
-      for j in range(9):
-        if(false_list[arr[j][k]]):
-          result = 0
-          break
-        else:
-          false_list[arr[j][k]] = True
+    if not is_valid_matrix(arr[j]):
+      result = 0
+      break
 
-  if(result = 1):
-    for k in range(9):
-      for j in range(9):
-        false_list = [False] * 9
-        if(false_list[arr[j][k]]):
-          result = 0
-          break
-        else:
-          false_list[arr[j][k]] = True
-      
+  if(result == 1):
+    tarr = transpose(arr)
+    for j in range(9):
+      if not is_valid_matrix(tarr[j]):
+        result = 0
+        break
 
-
-
+  if(result == 1):
+    matrix = []
+    for k in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            block = [num for row in arr[k:k+3] for num in row[j:j+3]]
+            matrix.append(block)
+    for j in range(9):
+      if not is_valid_matrix(matrix[j]):
+        result = 0
+        break
+    
   print("#", (i+1), " ", result, sep="")
