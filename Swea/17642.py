df6419caa443861885ddd9978fc41991985c2890
WaiCat <98246479+WaiCat@users.noreply.github.com>
2024-11-23 12:59:32 +0900

    Update 17642.py

--- Swea/17642.py
+++ Swea/17642.py
@@ -1,1 +1,12 @@
-print(-2 // 2)
+t = int(input())
+
+for i in range(t):
+    a, b = map(int, input().split())
+    count = 0
+    k = b-a
+    if(k < 0 or k == 1):
+        count = -1
+    else:
+        count += k // 2
+
+    print("#", (i+1), " ", count, sep="")
